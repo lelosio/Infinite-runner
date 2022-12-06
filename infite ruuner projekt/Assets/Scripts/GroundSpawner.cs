80a930463fe706282be49aca58e2ae0089f000ba
lelosio <113931027+lelosio@users.noreply.github.com>
2022-12-06 11:52:28 +0100

    prefaby + brakl postaci
    elo elo 320

---
+++ infite ruuner projekt/Assets/Scripts/GroundSpawner.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSpawner : MonoBehaviour
+{
+    public GameObject groundTile;
+    Vector3 nextSpawnPoint;
+    void Start()
+    {
+        
+    }
+
+    
+   
+
+}
