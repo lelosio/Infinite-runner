1ee135ce3ad2dac677d5a51c06941ca569424fa4
lelosio <113931027+lelosio@users.noreply.github.com>
2022-12-13 11:47:04 +0100

    Spwant + Groundtile
    odc 2/3 6:10

---
+++ infite ruuner projekt/Assets/Scripts/GroundTile.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class GroundTile : MonoBehaviour
+{
+    GroundSpawner groundSpawner;
+    private void Start()
+{
+        groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
+        SpawnObstacle();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        groundSpawner.SpawnTile();
+            Destroy(gameObject, 2);
+    }
+
+    private void Update()
+    {
+        
+    }
+    public GameObject obstaclePrefab;
+
+    void SpawnObstacle()
+    {
+        int obstacleSpwantIndex = Random.Range(2, 5);
+        Transform spwanPoint = transform.GetChild(obstacleSpwantIndex).transform;
+
+        Instantiate(obstaclePrefab, spwanPoint.position, Quaternion.identity, transform);
+    }
+
+}
